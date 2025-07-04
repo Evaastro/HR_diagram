a4c5aaf13421b27f0506c3be9a4d56a19e17356d
Evaastro <dearaujopereiraeva@gmail.com>
2025-07-04 09:19:02 +0200

    Prints are removed

--- HR_function.py
+++ HR_function.py
@@ -107,56 +107,33 @@ def add_star(ra=256.5229102004,dec=-26.5805651308,radius=1* u.arcsec):
     result=search.get_results()
     HR_plot(only_HR=False)
     if np.ma.is_masked(result['parallax'][0]):
-        return print("No gaia distances !")
-    print("Distance ",result['parallax'][0])
+        return print("No Gaia distances found !")
     if len(result)>0:
         for row in result:
-            print(f"source_id: {row['source_id']}")
-            print(f"G_mag: {row['phot_g_mean_mag']}")
-            print(f"BP_mag: {row['phot_bp_mean_mag']}")
-            print(f"RP_mag: {row['phot_rp_mean_mag']}")
-            print("----")
         A_G=result['ag_gspphot'][0]
-        print("A_G",A_G)
         Ebpminrp=result['ebpminrp_gspphot'][0]
         dist=1000/result['distance_gspphot'][0]
-        print("The distances ",dist)
         if np.ma.is_masked(dist):
-            print("Do not have any GSP distances")
             dist=result['parallax'][0]
-            print("The new distances ",dist)
         coord=SkyCoord(ra=ra, dec=dec, unit=(u.degree, u.degree),\
                        distance=1/dist*u.pc, frame='icrs')
         if np.ma.is_masked(A_G):
-            print("Do not have any Gaia GSP Absorption")
             lambda_G=5850.88 #G band
-            print("AG ici",A_G)
             A_G=Absorption(lambda_G, coord)
-            print("AG ici apres comp ",A_G)
         if np.ma.is_masked(Ebpminrp):
-            print("Do not have the GSP exctinction E(BP-RP)")
             lambda_BP=5041.61 #BP band
             lambda_RP=7690.74 #RP band
             A_BP=Absorption(lambda_BP, coord)
             A_RP=Absorption(lambda_RP, coord)
             Ebpminrp=A_BP-A_RP
-        print("E(BP-RP)=",Ebpminrp)
         color=result['bp_rp'][0]-Ebpminrp
-        print("absorption coefficient ",A_G)
-        print("color ",color)
-        print("distance gaia ",result['parallax'][0])
-        print("distance GSP ",1000/result['distance_gspphot'][0])
         mag_g_GSP=mag_abs(result['phot_g_mean_mag'][0],1000/result['distance_gspphot'][0],A_G)
         mag_g_gaia=mag_abs(result['phot_g_mean_mag'][0],result['parallax'][0],A_G)
         mag_gz=mag_abs(result['phot_g_mean_mag'][0],result['parallax'][0],0)
         mag_g=mag_abs(result['phot_g_mean_mag'][0],dist,A_G)
-        print("mag_g_GSP",mag_g_GSP)
-        print("mag_g_gaia",mag_g_gaia)
-        print("mag_gz",mag_gz)
-        print("Magnitude in the HR ",mag_g)
         plt.scatter(color, mag_g, marker='*', s=500,color='#3FE93F',edgecolor='black', linewidth=0.7)
     else:
-        print("Aucun objet Gaia trouvé à ces coordonnées.")
+        print("No Gaia object it is found at theses coordinates.")
     plt.show()
 
 print("\nProgramme terminer : HR function \n")
