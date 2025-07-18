25853923517ea93b37d1fe895f360eabaaec6a2d
Evaastro <dearaujopereiraeva@gmail.com>
2025-07-18 18:52:20 +0200

    Update HR_function.py

--- HR_function.py
+++ HR_function.py
@@ -16,7 +16,7 @@ from dustmaps.config import config
 from dust_extinction.parameter_averages import G23
 import dustmaps.bayestar
 from dustmaps.bayestar import BayestarQuery
-
+Gaia.MAIN_GAIA_TABLE="gaiadr3.gaia_source"
 
 plt.rcParams.update({
     'axes.titlesize': 16,
@@ -25,22 +25,6 @@ plt.rcParams.update({
     'ytick.labelsize': 14,
     'figure.dpi': 350
 })
-
-"""This is the query I use to create the file results gaia
-SELECT TOP 1000000
-parallax, bp_rp, phot_g_mean_mag,phot_bp_mean_mag,phot_rp_mean_mag,a_g_val, ra, dec
-FROM gaiadr2.gaia_source
-WHERE visibility_periods_used>8
-AND astrometric_chi2_al/(astrometric_n_good_obs_al-5)<1.44*greatest(1,exp(-0.4*(phot_g_mean_mag-19.5)))
-AND parallax_over_error>10
-AND phot_g_mean_flux_over_error>50
-AND phot_rp_mean_flux_over_error>20
-AND phot_bp_mean_flux_over_error>20
-AND phot_bp_rp_excess_factor > 1.0+0.015*power(phot_bp_mean_mag-phot_rp_mean_mag,2)
-AND phot_bp_rp_excess_factor < 1.3+0.06*power(phot_bp_mean_mag-phot_rp_mean_mag,2)
-AND 1000/parallax <= 200
-ORDER BY random_index
-"""
 
 chemin="results_gaia.csv"
 
